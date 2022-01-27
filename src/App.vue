90431777d70218d09c5c8585af35b3503ed16e59
Gal2o <hunmin8788@gmail.com>
2022-01-27 18:30:06 +0900

    feat: 11st clone coding

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
+  <LNB />
+  <RNB />
   <TheHeader />
-  <RouterView />
+  <Billboard />
+  <Direct />
+  <HotFocus />
 </template>
 
 <script>
+import LNB from '~/components/LNB'
+import RNB from '~/components/RNB'
 import TheHeader from '~/components/TheHeader'
-
+import Billboard from '~/components/Billboard'
+import Direct from '~/components/Direct'
+import HotFocus from '~/components/HotFocus'
 export default {
   components: {
-    TheHeader
+    LNB,
+    RNB,
+    TheHeader,
+    Billboard,
+    Direct,
+    HotFocus,
   }
 }
 </script>
+
+<style lang="scss">
+@import "~/scss/main";
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
 import App from '~/App.vue'
-import router from '~/routes'
 import store from '~/store'
+import fetch from '~/plugins/fetch'
+import search from '~/plugins/search'
+import lazyLoad from '~/plugins/lazyLoad'
 
 const app = createApp(App)
-app.use(router)
 app.use(store)
+app.use(fetch)
+app.use(search)
+app.use(lazyLoad)
 app.mount('#app')
